3587b16d2da02c4bd48e51b4559f5bbe3e1f8ddf
Likhithakanavu <likhithakanavu@gmail.com>
2023-07-19 10:54:08 +0530

    july 19

--- src/views/party/AddParty.js
+++ src/views/party/AddParty.js
@@ -96,7 +96,29 @@ return(
       </Box>
     </FormControl>
     
-
+    <FormControl>
+      <Box
+        sx={{
+          '& .MuiTextField-root': { m: 1, width: '50ch' }
+        }}
+      >
+           <InputLabel id="demo-simple-select-label">Unit of Measure</InputLabel>
+          <Select
+          onChange={Change} 
+            variant="outlined"
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            //   value={age}
+            name='unit_of_masure'
+            label="Tax Percentage"
+          >
+            <MenuItem value={10}>20%</MenuItem>
+            <MenuItem value={20}>10%</MenuItem>
+            <MenuItem value={30}>3%</MenuItem>
+          </Select>
+      
+      </Box>
+    </FormControl>
 
     <FormControl>
       <Box
@@ -104,6 +126,7 @@ return(
           '& .MuiTextField-root': { m: 1, width: '50ch' }
         }}
       >
+           
       
         <TextField id="outlined-basic" onChange={Change}  label="Party Country" name='party_country' variant="outlined" />
       </Box>
